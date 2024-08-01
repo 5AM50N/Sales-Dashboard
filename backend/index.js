bd6968b0532fd930d8a6cc273032efc956bad898
aditya <adityakamble.quicklabs1@gmail.com>
2024-08-01 19:54:00 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/router.js";
 import cors from 'cors';
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use("/products",router);
 app.use(express.json());
-mongoose.connect("mongodb+srv://adityakamblequicklabs1:IkTsH7nTmCEWadPo@cluster0.wopzecp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>app.listen(5000)).then(()=>console.log("connection successful")).catch((err)=>console.log(err));
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.mongo)
+.then(()=>app.listen(port)).then(()=>console.log("connection successful")).catch((err)=>console.log(err));
